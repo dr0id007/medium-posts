c31f21e70fb755a106f68c10f7371bb1e97c391b
Dev Arora <nullvoid.nv@gmail.com>
2020-07-04 13:02:19 +0530

    Delete prime.py

--- prime.py
+++
@@ -1,7 +0,0 @@
-# programme to check if a number is prime or not
-print('Enter a number to check')
-num = int(input('NUMBER : '))
-if num % 2 == 0:
-    print('The number is composite')
-else:
-    print('The number is prime')
